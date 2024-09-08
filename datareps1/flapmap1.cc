22570a523e908754a4f72d9528192ae2c60cba72
Reza Shamji <rezashamji@college.harvard.edu>
2024-09-08 15:44:30 -0400

    notes

--- datareps1/flapmap1.cc
+++ datareps1/flapmap1.cc
@@ -2,13 +2,34 @@
 
 void add_sample(uintptr_t start, size_t duration, size_t flapcount) {
     // Placeholders to avoid compiler warnings. Replace them with your code.
-    (void) start, (void) duration, (void) flapcount; 
+    assert(true);
+    assert(duration>0); //will throw runtime error if this isn't true
+    auto it = flapmap.insert({start, {.start=start, .duration=duration, .flapcount=flapcount}});
+
+    //try to coalesce
+    if (can_coalesce_up())
+
+    //(void) start, (void) duration, (void) flapcount;  //void in paren and variable names is because C++ does bring up warnings when compile
 }
 
 bool has_sample(uintptr_t t) {
     // Placeholders to avoid compiler warnings. Replace them with your code.
-    (void) t; 
-	return false;
+    //(void) t; 
+    for (auto it = flapmap.begin(); it!= flapmap.end(); it++) {
+        if (it ->second.start <= t && it->second.start + it->second.duration >t) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool has_sample2(uintptr_t t) {
+    auto it = flapmap.upper_bound(t).;
+    if (it == flapmap.begin()) {
+        return false;
+    }
+    it--;
+    return it->second.start <= t && it->second.start + it->second.duration >t;
 }
 
 bool sample_overlaps(uintptr_t start, size_t duration) {
@@ -19,13 +40,28 @@ bool sample_overlaps(uintptr_t start, size_t duration) {
 
 bool can_coalesce_up(flapmap_iter it) {
     // Placeholders to avoid compiler warnings. Replace them with your code.
-    (void) it; 
-	return false;
+    //void) it; 
+    auto next = it;
+    next++;
+    if (next == flapmap.end()) {
+        return false;
+    }
+    //key: it-> first
+    //value: it->second
+    assert(it -> first == it->second.start)
+    // returns true if the current sample is aligned to the next one
+    // will use in pset
+	return it->second.start + it->second.duration == next->second.start
 }
 
 void coalesce_up(flapmap_iter it) {
     // Placeholders to avoid compiler warnings. Replace them with your code.
-    (void) it; 
+    //(void) it; 
+    assert (can_coalesce_up(it))
+    auto next = it;
+    next++;
+    it->second.duration += next->second.duration;
+    flapmap.erase(next);
 }
 
 bool can_coalesce_down(flapmap_iter it) {
